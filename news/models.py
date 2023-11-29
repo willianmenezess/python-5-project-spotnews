124fdff0085fa711173875bd702bdd9475f8338a
willianmenezess <willmenezes@outlook.com.br>
2023-11-29 19:41:08 -0300

    feat: resolve requisito 2

--- news/models.py
+++ news/models.py
@@ -7,3 +7,14 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class User(models.Model):
+    name = models.CharField(max_length=200, validators=[validate_name])
+    email = models.EmailField(
+        max_length=200, unique=True, validators=[validate_name])
+    password = models.CharField(max_length=200)
+    role = models.CharField(max_length=200, validators=[validate_name])
+
+    def __str__(self):
+        return self.name
