61a9c3f52ccb3574568ca48213f253431571b32b
willianmenezess <willmenezes@outlook.com.br>
2023-11-30 19:43:44 -0300

    feat: resolve requisito 4

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from news.models import Category, User, News
 
-# Register your models here.
+admin.site.site_header = 'Admin Spot News'
+admin.site.register(Category)
+admin.site.register(User)
+admin.site.register(News)
--- news/views.py
+++ news/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from news.models import News
 
-# Create your views here.
+
+def home(request):
+    news = News.objects.all()
+    context = {'news': news}
+    return render(request, 'home.html', context)
