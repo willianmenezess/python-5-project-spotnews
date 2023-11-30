f03c1a05e76e947136c1d3fe537d99556c7c20e0
willianmenezess <willmenezes@outlook.com.br>
2023-11-30 10:54:19 -0300

    feat: resolve requisito 3

--- news/models.py
+++ news/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from news.validators import validate_name
+from news.validators import (
+    validate_name, validate_empty, validate_title)
 
 
 class Category(models.Model):
@@ -18,3 +19,16 @@ class User(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class News(models.Model):
+    title = models.CharField(
+        max_length=200, validators=[validate_empty, validate_title])
+    content = models.TextField(validators=[validate_empty])
+    categories = models.ManyToManyField(Category, related_name="news")  # N:N
+    author = models.ForeignKey(User, on_delete=models.CASCADE)  # 1:1
+    image = models.ImageField(upload_to='img/', null=True, blank=True)
+    created_at = models.DateField()
+
+    def __str__(self):
+        return self.title
--- news/validators.py
+++ news/validators.py
@@ -6,3 +6,14 @@ def validate_name(value):
         raise ValidationError(
             'Certifique-se de que o valor tenha no máximo 200'
             f' caracteres (ele possui {len(value)}).')
+
+
+def validate_title(value):
+    list_of_words = value.split(" ")
+    if len(list_of_words) < 2:
+        raise ValidationError('O título deve conter pelo menos 2 palavras.')
+
+
+def validate_empty(value):
+    if len(value) == 0:
+        raise ValidationError('O campo não pode estar vazio')
