0a9b373782995b36c51c5860efaf03ef0fb12376
willianmenezess <willmenezes@outlook.com.br>
2023-11-30 23:19:14 -0300

    feat: resolve requisito 5

--- news/urls.py
+++ news/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from news.views import home
+from news.views import home, news_details
 
 
 urlpatterns = [
     path('', home, name='home-page'),
+    path('news/<int:id>/', news_details, name='news-details-page'),
 ]
--- news/views.py
+++ news/views.py
@@ -6,3 +6,9 @@ def home(request):
     news = News.objects.all()
     context = {'news': news}
     return render(request, 'home.html', context)
+
+
+def news_details(request, id):
+    new = News.objects.get(id=id)
+    context = {'new': new}
+    return render(request, 'news_details.html', context)
