bc1e483ab96e144fdb3869025dce778c5021862c
willianmenezess <willmenezes@outlook.com.br>
2023-12-02 17:16:47 -0300

    feat: resolve requisito 10

--- spotnews/urls.py
+++ spotnews/urls.py
@@ -21,10 +21,12 @@ from django.conf.urls.static import static
 from rest_framework import routers
 from news_rest.views.category_view import CategoryViewSet
 from news_rest.views.user_view import UserViewSet
+from news_rest.views.news_view import NewsViewSet
 
 router = routers.DefaultRouter()
 router.register(r"categories", CategoryViewSet)
 router.register(r"users", UserViewSet)
+router.register(r"news", NewsViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
